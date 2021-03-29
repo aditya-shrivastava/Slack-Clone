b0f06ad006fba5aa82dd5b5f65062fd597bb067f
Aditya-Shrivastava <39914834+Aditya-Shrivastava@users.noreply.github.com>
2021-03-29 21:26:03 +0530

    add Header

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyCUGqthAx1Yn6WmOH_iM_mSe_rt-RqvthA',
+	authDomain: 'hatchways-frontend.firebaseapp.com',
+	projectId: 'hatchways-frontend',
+	storageBucket: 'hatchways-frontend.appspot.com',
+	messagingSenderId: '681752486278',
+	appId: '1:681752486278:web:363b5070c6d8a409830c07',
+};
